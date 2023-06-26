e2c3c06ffb5eec74c64561d329386a135f27dd52
eslamfaisal <eslamfaisal423@gmail.com>
2023-06-26 17:27:04 +0300

    fix cors headers for swagger profuction

--- src/main/kotlin/site/bluethunder/plugins/GlobalPlugin.kt
+++ src/main/kotlin/site/bluethunder/plugins/GlobalPlugin.kt
@@ -18,13 +18,33 @@ fun Application.configureBasic() {
 //        anyHost()
 //    }
     install(CORS) {
-        HttpMethod.DefaultMethods.forEach { allowHeader(it.value) }
-        allowHeaders { true }
+        anyHost()  // Allow requests from any host
         allowCredentials = true
-        anyHost()
-        allowHost("0.0.0.0:8080", schemes = listOf("https","http"))
-        allowHost("free.bluethunder.site", schemes = listOf("https","http"))
+        allowNonSimpleContentTypes = true  // Allows all headers, including non-simple ones
+        allowHeader(HttpHeaders.AccessControlAllowHeaders)  // Allows setting of any headers
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
     }
+
+//
+//    install(CORS) {
+//        allowMethod(HttpMethod.Options)
+//        allowMethod(HttpMethod.Post)
+//        allowMethod(HttpMethod.Get)
+//        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+//        allowHeader(HttpHeaders.ContentType)
+//        allowHeader(HttpHeaders.Authorization)
+//        HttpMethod.DefaultMethods.forEach { allowHeader(it.value) }
+//        allowHeaders { true }
+//        allowCredentials = true
+//        anyHost()
+//        allowHost("0.0.0.0:8080", schemes = listOf("https", "http"))
+//        allowHost("free.bluethunder.site", schemes = listOf("https", "http"))
+//    }
 
     install(CallLogging)
     install(ContentNegotiation) {
@@ -47,9 +67,15 @@ fun Application.configureBasic() {
         }
         // describe the server, add as many as you want
 //        server("http://0.0.0.0:8080/") {
+//            description = "Ktor for production server"
+//        }
         server("https://free.bluethunder.site/") {
             description = "Ktor for production server"
         }
+//        server("http://free.bluethunder.site/") {
+//            description = "Ktor for production server"
+//        }
+
         //optional custom schema object name
         replaceModule(DefaultSchemaNamer, object : SchemaNamer {
             val regex = Regex("[A-Za-z0-9_.]+")
